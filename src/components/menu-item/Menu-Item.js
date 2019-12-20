8e22927ebf2fb362ae51f98c46bf823fcbfaf2af
MatJ <jurek.mateusz89@gmail.com>
2019-12-20 07:48:12 +0100

    added more comments

--- src/components/menu-item/Menu-Item.js
+++ src/components/menu-item/Menu-Item.js
@@ -7,10 +7,13 @@ import YouTube from "react-youtube";
 
 class MenuItem extends React.Component {
   state = {
-    toggle: false
+    toggle: false,
+    disabled: false
   };
   //need to fix unique key error in console
+  //need to make other objects disabled when current active
   render() {
+    // options for youtube player
     const opts = {
       height: "200",
       width: "200",
@@ -23,24 +26,38 @@ class MenuItem extends React.Component {
       strMeal,
       strMealThumb,
       strYoutube,
-      strInstructions
+      strInstructions,
+      strIngredient1,
+      strIngredient2,
+      strIngredient3,
+      strIngredient4,
+      strIngredient5
     } = this.props.el;
     let accordion = "";
 
     if (this.state.toggle === true) {
       accordion = (
-        <Media>
-          <Media left href="#">
+        <div className="row">
+          <div className="col-4">
             <YouTube
+              className="mr-2 mb-0 pb-o"
               videoId={strYoutube.slice(strYoutube.length - 11)}
               opts={opts}
             />
-          </Media>
-          <Media body>
-            <Media heading>{strMeal}</Media>
-            {strInstructions}
-          </Media>
-        </Media>
+            <h4>Ingredients:</h4>
+            <ul>
+              <li>{strIngredient1}</li>
+              <li>{strIngredient2}</li>
+              <li>{strIngredient3}</li>
+              <li>{strIngredient4}</li>
+              <li>{strIngredient5}</li>
+            </ul>
+          </div>
+          <div className="col-8">
+            <h3>{strMeal}</h3>
+            <p>{strInstructions}</p>
+          </div>
+        </div>
       );
     } else {
       accordion = "";
@@ -52,7 +69,8 @@ class MenuItem extends React.Component {
           <div
             className="background-image"
             style={{
-              backgroundImage: `url(${strMealThumb})`
+              backgroundImage: `url(${strMealThumb})`,
+              disabled: true
             }}
             onClick={() =>
               this.setState(prevState => ({
